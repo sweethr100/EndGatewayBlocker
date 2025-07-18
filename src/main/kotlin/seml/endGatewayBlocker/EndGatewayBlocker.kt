c5eff626ea318a25a1096f96df91883783221774
sweethr100 <sweethr100@naver.com>
2025-07-18 20:41:20 +0900

    메시지 사용자 설정 가능

--- src/main/kotlin/seml/endGatewayBlocker/EndGatewayBlocker.kt
+++ src/main/kotlin/seml/endGatewayBlocker/EndGatewayBlocker.kt
@@ -6,12 +6,20 @@ import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.PortalType
 import org.bukkit.advancement.Advancement
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.event.entity.EntityPortalEnterEvent
 import org.bukkit.event.player.PlayerAdvancementDoneEvent
+import org.bukkit.ChatColor
 
 class EndGatewayBlocker : JavaPlugin(), Listener {
+    private lateinit var config: FileConfiguration
+    private var blockMessage: String = "엔드 게이트웨이가 차단되었습니다."  // 기본 메시지
 
     override fun onEnable() {
+        saveDefaultConfig()
+        config = getConfig()
+        blockMessage = ChatColor.translateAlternateColorCodes('&', config.getString("block-message", blockMessage)!!)
+
         server.pluginManager.registerEvents(this, this)
     }
 
@@ -22,7 +30,7 @@ class EndGatewayBlocker : JavaPlugin(), Listener {
             event.isCancelled = true
             // 플레이어에게 메시지 전송 (선택사항)
             if (event.entity is org.bukkit.entity.Player) {
-                (event.entity as org.bukkit.entity.Player).sendActionBar("§c엔드 게이트웨이가 차단되었습니다.")  // 액션바 메시지 (색상 추가 예시)  // 액션바 메시지 (색상 추가 예시)
+                (event.entity as org.bukkit.entity.Player).sendMessage(blockMessage)
             }
         }
     }
